daaa0f764511833f2189da975adae72d049a0ae1
juliadiasbatista <JULIABATISTA@bandtec.com.br>
2021-12-01 23:17:36 -0300

    site-individual

---
+++ Site ju/src/database/sql.sql
@@ -0,0 +1,13 @@
+create database astrolylearn;
+use astrolylearn;
+
+CREATE TABLE usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+
+
+
---
+++ Site ju/src/models/usuarioModel.js
@@ -0,0 +1,44 @@
+var database = require("../database/config")
+
+function contagemSignos(fkSigno){
+
+var instrucao = `select count(*) as quantidade from usuario where fk_signo = ${fkSigno}`;
+
+return database.executar(instrucao);
+// A função esta retornando o resultado do select
+
+}
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrar(nome, email, senha, signo) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    var instrucao = `
+        INSERT INTO usuario (nome, email, senha, fk_signo) VALUES ('${nome}', '${email}', '${senha}', '${signo}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+    contagemSignos,
+};
